f4d0a7871b5d70b18b401a6c153cf3ae473566e6
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2025-02-21 15:45:04 +0000

    fix

--- examples.py
+++ examples.py
@@ -1,10 +1,11 @@
 from Modules import *
 
 model = ruby_pipeline(
-    fork(),
-    [ Id(), seq(shape_transform_1d(1, 10), activation("relu")) ],
-    fst(lambda x: x * 2),
-    [(activation("relu"), activation("sigmoid"))]
+    fork(),                                      # x           -> [x, x]
+    [ Id(), seq(shape_transform_1d(1, 10)) ],    # [x, x]      -> [x, x']
+    fst(lambda x: x * 2),                        # [x, x']     -> [x * 2, x']
+    [activation("relu"), activation("sigmoid")], # [x * 2, x'] -> [relu(x * 2), sigmoid(x')]
+    pick_at(0),                                  # [relu(x * 2), sigmoid(x')] -> relu(x * 2)
 )
 
 print(model)
